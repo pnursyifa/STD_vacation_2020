4a997ebf8f2e5ac60f7ca025857f465bf6d77d67
Dindin14 <62365695+Dindin14@users.noreply.github.com>
2020-03-29 12:39:43 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
-#include "vacation.h"
+#include "parents1.h"
+#include <iostream>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    address P;
+    address Prec;
+
+    List L;
+
+    createList(L);
+    createNewElmt('Ragunan', P);
+    insertFirst(L, P);
+    printInfo(L);
     return 0;
 }
